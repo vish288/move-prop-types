83ab56f8e75a630118e9aa70e9749f4b4acc9158
Vishweshwaran Suri <v5.itsme@gmail.com>
2017-09-14 01:12:22 +0530

    make it runnable

---
+++ src/updateFile.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const fileName = process.argv.splice(2)[0];
+fs.readFile(fileName, 'utf-8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  const result = `#!/usr/bin/env node \n${data}`;
+
+  fs.writeFile(fileName, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
