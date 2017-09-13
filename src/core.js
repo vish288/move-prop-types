67c4fbef9a2622f134ed2191b90db6a85ce5eb2c
Visweshwaran S <vsuryanarayanan@sapient.com>
2017-09-13 19:52:49 +0530

    further changes

--- src/core.js
+++ src/core.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 const propReplace = require('commander');
-const {argv} = require('process');
+const { argv } = require('process');
 
-const {installPackage, updateFile, updateFolder, helpExamples, findMatch} = require('./helper');
+const { installPackage, updateFile, updateFolder, helpExamples, findMatch } = require('./helper');
 const packageJson = require('../package.json');
 
 // const options = `${propReplace.install ? 'install' : ''} and${(propReplace.path ? ' single file with relative path' : '') || (propReplace.folder ? ' folder ' + process.argv + ' of js(x) files' : '')}`;
@@ -16,24 +16,26 @@ const packageJson = require('../package.json');
 
 // No or unknown options given, will trigger help text
 if (!(argv.indexOf('--install') !== -1 || argv.indexOf('-I') !== -1) &&
-    !(argv.indexOf('--path') !== -1 || argv.indexOf('-P') !== -1) &&
-    !(argv.indexOf('--folder') !== -1 || argv.indexOf('-F') !== -1)) {
+  !(argv.indexOf('--path') !== -1 || argv.indexOf('-P') !== -1) &&
+  !(argv.indexOf('--folder') !== -1 || argv.indexOf('-F') !== -1)) {
   argv.push('--help');
 }
 
-const filePath = findMatch(argv, ['--path', '-P']);
+const filePath = findMatch(argv, [ '--path', '-P' ]);
 
-const folderPath = findMatch(argv, ['--folder', '-F']);
+const folderPath = findMatch(argv, [ '--folder', '-F' ]);
 
 propReplace
   .command('move-prop-types')
   .alias('mpt')
   .usage('[options] [file|folder]')
   .version(`${packageJson.version}`)
-  .option('-I, --install', 'install the latest proptypes and then continue with rest of the commands', installPackage, {cmd: process.argv})
+  .option('-I, --install', 'install the latest proptypes and then continue with rest of the commands', installPackage, { cmd: process.argv })
   .option('-P, --path', 'input path information of the file to update', updateFile, filePath)
   .option('-F, --folder', 'input folder info where all the files would be updated', updateFolder, folderPath)
   .on('--help , -H', helpExamples)
   .parse(argv);
+
+propReplace.cli = true;
 
 exports.module = propReplace;
