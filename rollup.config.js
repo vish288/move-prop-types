ff940e43e7707d267bf102abb78256a4522cefb6
Vishweshwaran Suri <v5.itsme@gmail.com>
2017-12-25 13:32:53 +0530

    update to the dependency packages

--- rollup.config.js
+++ rollup.config.js
@@ -16,24 +16,23 @@ export default {
 			presets: ['es2015-rollup', 'stage-0', 'stage-1']
 		}),
 		resolve({
-
 			// use "jsnext:main" if possible
 			// – see https://github.com/rollup/rollup/wiki/jsnext:main
-			jsnext: true,  // Default: false
+			jsnext: true, // Default: false
 
 			// use "main" field or index.js, even if it's not an ES6 module
 			// (needs to be converted from CommonJS to ES6
 			// – see https://github.com/rollup/rollup-plugin-commonjs
-			main: true,  // Default: true
+			main: true, // Default: true
 		}),
 		globals(),
 		builtins(),
 		uglify()
 	],
 	context: 'global',
-	name: 'movePropTypes',
 	output: {
 		file: 'build/mpt.js',
-		format: 'cjs'
+		format: 'cjs',
+		name: 'movePropTypes',
 	}
 };
